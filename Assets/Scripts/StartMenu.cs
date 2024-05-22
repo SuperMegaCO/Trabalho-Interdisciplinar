d9dcb93e0677e6211aba787201b1470f8b20a569
SuperMegaCO <rafacortat@gmail.com>
2024-05-22 19:17:59 -0300

    StartMenus

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class StartMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     public int LevelSelect = 0;
+    public Canvas StartScreen;
+    public Canvas LevelScreen;
+    public Canvas PauseScreen;
     void Start()
     {
         AudioController.Instance.TocarBGMusic(0);
@@ -17,26 +21,48 @@ public class StartMenu : MonoBehaviour
     {
         
     }
-    public void LevelOne()
-    {
-        LevelSelect = 1;
-    }
-    public void LevelTwo()
-    {
-        LevelSelect = 2;
-    }
+
     public void StartGame() 
     {
         SceneManager.LoadScene("Level 1", LoadSceneMode.Single);
         AudioController.Instance.TocarBGMusic(1);
     }
-    public void StartGameFromSelect()
+    public void StartGameFromSelectOne()
     {
-        SceneManager.LoadScene($"Level {LevelSelect}", LoadSceneMode.Single);
+        SceneManager.LoadScene($"Level 1", LoadSceneMode.Single);
         AudioController.Instance.TocarBGMusic(1);
     }
-    
-        
-    
+    public void StartGameFromSelectTwo()
+    {
+        SceneManager.LoadScene($"Level 2", LoadSceneMode.Single);
+        AudioController.Instance.TocarBGMusic(2);
+    }
+    public void StartGameFromSelectThree()
+    {
+        SceneManager.LoadScene($"Level 3", LoadSceneMode.Single);
+        AudioController.Instance.TocarBGMusic(1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void GoToLvlSelect()
+    {
+        StartScreen.gameObject.SetActive(false); 
+        LevelScreen.gameObject.SetActive(true);
+    }
+    public void GoToStart()
+    {
+        StartScreen.gameObject.SetActive(true);
+        LevelScreen.gameObject.SetActive(false);
+    }
+    public void GoToPause()
+    {
+        StartScreen.gameObject.SetActive(true);
+        LevelScreen.gameObject.SetActive(false);
+    }
+
+
+
 
 }
